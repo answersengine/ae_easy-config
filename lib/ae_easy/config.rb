be59c31916ee219c3007c45725c67c3c7ae529b4
colorfulsing <47130084+colorfulsing@users.noreply.github.com>
2024-01-15 12:55:43 -0600

    Prioritize ae_easy.yaml when using ae_easy-config alias instead of dh_easy.yaml

--- lib/ae_easy/config.rb
+++ lib/ae_easy/config.rb
@@ -1,8 +1,8 @@
 require 'dh_easy/config'
 require 'ae_easy/core'
 
-DhEasy::Config::Local.default_file_path_list << 'ae_easy.yaml'
-DhEasy::Config::Local.default_file_path_list << 'ae_easy.yml'
+DhEasy::Config::Local.default_file_path_list.insert(0, 'ae_easy.yml')
+DhEasy::Config::Local.default_file_path_list.insert(0, 'ae_easy.yaml')
 
 # (Deprecated) Alias to DhEasy module.
 AeEasy = ::DhEasy unless defined? ::AeEasy
